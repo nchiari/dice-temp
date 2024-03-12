b0f22b8ee4522b1ba5d25a3fd62a9a7417810b09
Nico Chiari <nchiari@gmail.com>
2024-03-12 11:53:08 +0100

    added title and button

--- scriptv1.js
+++ scriptv1.js
@@ -1,14 +1,15 @@
 const dice = document.getElementById("dice")
+const rollButton = document.getElementById("rollButton") // Obtiene el botón por su ID
 
-dice.addEventListener("click", () => {
-  // Números aleatorios para las rotaciones en X, Y, y Z
-  const xRand = getRandomInt(1, 16) * 90 // Asegúrate de que sea múltiplo de 90
+function rollDice() {
+  const xRand = getRandomInt(1, 16) * 90
   const yRand = getRandomInt(1, 16) * 90
-  const zRand = getRandomInt(1, 16) * 90 // La rotación en Z es opcional y puede hacer que el dado parezca más dinámico
+  const zRand = getRandomInt(1, 16) * 90
 
-  // Aplicar rotaciones al dado
   dice.style.transform = `rotateX(${xRand}deg) rotateY(${yRand}deg) rotateZ(${zRand}deg)`
-})
+}
+// Event listener para el botón
+rollButton.addEventListener("click", rollDice)
 
 // Función para obtener un número entero aleatorio
 function getRandomInt(min, max) {
